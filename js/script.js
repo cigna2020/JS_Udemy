a8a3baf12e5dc033b38b109b8062196592a9d80a
Alex <cigan@ua.fm>
2020-09-14 10:48:55 +0300

    make examples with the operator "if", "switch" and "тернарный оператор"

--- js/script.js
+++ js/script.js
@@ -1,19 +1,57 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//   b = prompt("На сколько оцените его?", ""),
+//   c = prompt("Один из последних просмотренных фильмов?", ""),
+//   d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// ***************ОПЕРАТОР if*******************
+
+// if ('4 != 9') {
+//   console.log("Ok!");
+// } else {
+//   console.log("Error!");
+// }
+
+// const num = 51;
+// if (num < 49) {
+//   console.log("Error");
+// } else if (num > 50) {
+//   console.log("too much");
+// } else if (num == 50) {
+//   console.log("Ok!");
+// };
+
+// *******************ТЕРНАРНЫЙ ОПЕРАТОР*******************
+// const num = 51;
+// num == 50 ? console.log("Ok!") : console.log("Error!");
+
+// ********************ОПЕРАТОР switch******************
+// const num = 9;
+// switch (num) {
+//   case 49:
+//     console.log("litle");
+//     break;
+//   case 51:
+//     console.log("too much");
+//     break;
+//   case 50:
+//     console.log("Ok!");
+//     break;
+//   default:
+//     console.log("Error!");
+//     break;
+// }
