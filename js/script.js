dac3db66d332a73240690055afb3a6dfc74a73db
Alex <cigan@ua.fm>
2020-09-15 11:39:45 +0300

    functions, methods and properties

--- js/script.js
+++ js/script.js
@@ -89,3 +89,107 @@
 //   }
 //   console.log(i);
 // }
+
+/* Задание на урок:
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", "");
+//     if (
+//         a != null &&
+//         b != null &&
+//         a != "" &&
+//         b != "" &&
+//         a.length < 10 &&
+//         b.length < 10
+//     ) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//     } else {
+//         console.log("Error!");
+//         i--;
+//     }
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+// } else {
+//     console.log("Произошла ошибка");
+// }
+
+// console.log(personalMovieDB);
+
+
+// **********************function********************
+
+// let num = 30;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 20;
+// }
+
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+// function culc(a, b) {
+//     return (a + b);
+// }
+// console.log(culc(5, 7));
+
+// let num = 30;
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// ******************  Свойства и Методы ***********************
+// const str = 'stroca fruit';
+// console.log(str.length);
+// console.log(str[2]);
+
+// const strUpperCase = str.toUpperCase();
+// // console.log(str.toUpperCase());
+// console.log(str);
+// console.log(strUpperCase);
+// console.log(strUpperCase.toLowerCase());
+// console.log(str.indexOf('q'));
+// console.log(str.slice(1, 7));
+// console.log(str.substring(1, 7));
+// console.log(str);
+
+// const num = 12.6;
+// console.log(Math.round(num));
+// console.log(num);
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
