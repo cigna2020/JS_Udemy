e6c2f15e4160429a5d8a5fef677830c47c10bef5
Alex <cigan@ua.fm>
2020-09-21 16:26:06 +0300

    part 3, lesson 40, work with date.

---
+++ script_data.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// const now = new Date();
+
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+// console.log(now.setHours(10));
+// console.log(now);
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+console.log(`cycle has worked ${(end - start) / 60}`); 
