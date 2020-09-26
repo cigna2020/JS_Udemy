e2cd8f0edbf490844af1b143d864a847d724c887
Alex <cigan@ua.fm>
2020-09-26 18:52:33 +0300

    part 4, lesson 66, make calculator of weight, p.1

--- js/script.js
+++ js/script.js
@@ -602,16 +602,21 @@ document.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     }
 
-
-
     //===================================================    
+
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
         // таким образом с напр. 500рх получаем число 500
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        // if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        // if (offset == width.replace(/\D/g, '') * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {                 // тоже самое, что и выше, но через новсозданую функцию
             offset = 0;
         } else {
             // таким образом с напр. 500рх получаем число 500
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -634,9 +639,9 @@ document.addEventListener('DOMContentLoaded', () => {
     prev.addEventListener('click', () => {
         // таким образом с напр. 500рх получаем число 500
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+            offset = deleteNotDigits(width) * (slides.length - 1)
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -663,7 +668,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');             //получаем число
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
             slidesField.style.transform = `translateX(-${offset}px)`;
 
             if (slides.length < 10) {
@@ -675,5 +680,74 @@ document.addEventListener('DOMContentLoaded', () => {
             dots[slideIndex - 1].style.opacity = 1;
         })
     })
+
+
+    // КАЛЬКУЛЯТОР суточной нормы калорий
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', height, weight, age, ratio = 1.375;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '_______';
+            return;                                         // что-бы прервать подальшее выполнение функции
+        }
+
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+
+                e.target.classList.add(activeClass);
+
+                calcTotal();
+            });
+        })
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch (input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 
 });
