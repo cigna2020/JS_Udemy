3c12b497cac79df5f3b05190be9822d8639ab464
Alex <cigan@ua.fm>
2020-09-28 10:56:55 +0300

    part 4, lesson 70, about module

--- lesson_69_encapsulation/lesson_69_encapsulation.js
+++ lesson_69_encapsulation/lesson_69_encapsulation.js
@@ -37,36 +37,48 @@
 // ivan.say();
 
 
-class User {
-    constructor(name, age) {
-        this.name = name;
-        this._age = age;
-    }
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this._age = age;
+//     }
 
-    #surname = 'Tsyhan';         // приватное свойство класса, к нему нельзя достучатся из вне
+//     #surname = 'Tsyhan';         // приватное свойство класса, к нему нельзя достучатся из вне
 
-    say() {
-        console.log(`Name of user: ${this.name} ${this.#surname}, Age: ${this._age} `);
-    }
+//     say() {
+//         console.log(`Name of user: ${this.name} ${this.#surname}, Age: ${this._age} `);
+//     }
 
-    get age() {
-        return this._age;
-    }
+//     get age() {
+//         return this._age;
+//     }
 
-    set age(age) {
-        if (typeof age === 'number' && age > 0 && age < 110) {
-            this._age = age;
-        } else {
-            console.log('Error!!!')
-        }
-    }
-}
+//     set age(age) {
+//         if (typeof age === 'number' && age > 0 && age < 110) {
+//             this._age = age;
+//         } else {
+//             console.log('Error!!!')
+//         }
+//     }
+// }
 
-const ivan = new User('Ivan', 27);
+// const ivan = new User('Ivan', 27);
 
-console.log(ivan.age);
-ivan.age = 99;
-console.log(ivan.age);
+// console.log(ivan.age);
+// ivan.age = 99;
+// console.log(ivan.age);
 
-ivan.say();         // в этом методе surname будет Tsyhan
-console.log(ivan.surname);  // surname - underfined, так как пытаемся из вне
+// ivan.say();         // в этом методе surname будет Tsyhan
+// console.log(ivan.surname);  // surname - underfined, так как пытаемся из вне
+
+const user = (function () {
+    const privat = function () {
+        console.log('I am privat!');
+    };
+
+    return {
+        sayHello: privat            // Нужно возвращать именно, как метод, иначе нет доступа
+    };
+}());
+
+user.sayHello();
